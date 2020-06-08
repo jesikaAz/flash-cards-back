c7a354cb7794ad508d2ba612c752edf1949dddfb
jesikaAz <j.anschutz@laposte.net>
2020-06-08 15:06:21 +0200

    add service method + interface

--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Card } from 'src/interfaces/card.interface';
 
 @Injectable()
-export class CardsService {}
+export class CardsService {
+    getCards():Promise<Card[]> {
+        const cards = [
+            {id:1, question:'Question1?', answer:'Response1'},
+            {id:2, question:'Question2?', answer:'Response2'}
+        ];
+        return new Promise(resolve => {
+            resolve(cards);
+        })
+    }
+}
